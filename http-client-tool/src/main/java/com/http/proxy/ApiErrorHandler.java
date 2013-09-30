5d096c512c57c4e9cdfd3f61e6c78148045483f5
Mauro Giorda <mgiorda@apigee.com>
2013-09-30 10:47:55 -0300

    Project organizing: removing unrelated things

--- http-client-tool/src/main/java/com/http/proxy/ApiErrorHandler.java
+++
@@ -1,8 +0,0 @@
-package com.http.proxy;
-
-public interface ApiErrorHandler<T> {
-
-	Class<T> getErrorType();
-
-	void throwException(Object errorObject) throws Exception;
-}
--- http-client-tool/src/main/java/com/http/proxy/ApiMethodHandler.java
+++
@@ -1,8 +0,0 @@
-package com.http.proxy;
-
-import java.lang.reflect.Method;
-
-public interface ApiMethodHandler {
-
-	Object handleMethodCall(Method method, Object[] args) throws Exception;
-}
